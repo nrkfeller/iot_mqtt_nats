e35d1b29af3455b7eac8c176c68555954bc7a4e6
NickFeller <nrkfeller@gmail.com>
2016-07-05 16:21:48 -0400

    adding mqtt cl messenger

---
+++ MQTT/gochatmqtt.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
+)
+
+func messageReceived(client *MQTT.Client, msg MQTT.Message) {
+	topics := strings.Split(msg.Topic(), "/")
+	msgFrom := topics[len(topics)-1]
+	fmt.Print(msgFrom + ": " + string(msg.Payload()))
+}
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	rand.Seed(time.Now().Unix())
+
+	server := flag.String("server", "tcp://iot.eclipse.org:1883", "The MQTT server to connect to")
+	room := flag.String("room", "gochat", "The chat room to enter. default 'gochat'")
+	name := flag.String("name", "user"+strconv.Itoa(rand.Intn(1000)), "Username to be displayed")
+	flag.Parse()
+
+	subTopic := strings.Join([]string{"/gochat/", *room, "/+"}, "")
+	pubTopic := strings.Join([]string{"/gochat/", *room, "/", *name}, "")
+
+	opts := MQTT.NewClientOptions().AddBroker(*server).SetClientID(*name).SetCleanSession(true)
+
+	opts.OnConnect = func(c *MQTT.Client) {
+		if token := c.Subscribe(subTopic, 1, messageReceived); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+	}
+
+	client := MQTT.NewClient(opts)
+
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	fmt.Printf("Connected as %s to %s\n", *name, *server)
+
+	for {
+		message, err := stdin.ReadString('\n')
+		if err == io.EOF {
+			os.Exit(0)
+		}
+		if token := client.Publish(pubTopic, 1, false, message); token.Wait() && token.Error() != nil {
+			fmt.Println("Failed to send message")
+		}
+	}
+}
